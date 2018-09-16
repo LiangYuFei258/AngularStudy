98b72183045c2d5087d91c090b03a6452e428722
yufei liang <liangyufei258@outlook.com>
2018-09-17 00:39:22 +0800

    updateCode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,15 @@ import { FooterComponent } from './footer/footer.component';
 import { SearchComponent } from './search/search.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { ProductComponent } from './product/product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductService } from './shared/product.service';
+
+const routeConfig: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'product/:productId', component: ProductDetailComponent }
+];
 
 @NgModule({
   declarations: [
@@ -17,12 +26,15 @@ import { ProductComponent } from './product/product.component';
     FooterComponent,
     SearchComponent,
     CarouselComponent,
-    ProductComponent
+    ProductComponent,
+    ProductDetailComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routeConfig)
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-stars',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stars.component.css']
 })
 export class StarsComponent implements OnInit {
+  @Input()
+  private rating: number;
+  private stars: string[];
 
   constructor() { }
 
   ngOnInit() {
+    this.stars = [];
+    for (let i = 0; i < 5; i++) {
+      const temNumber = this.rating - i;
+      if (temNumber >= 1) {
+        this.stars.push('star');
+      } if (temNumber === 0.5) {
+        this.stars.push('star_half');
+      } if (temNumber <= 0) {
+        this.stars.push('star_border');
+      }
+    }
   }
 
 }
